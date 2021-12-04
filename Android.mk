ef69679144fe353d6206965832a05e11c6299d20
Adam Lawson <adamjlawson22@gmail.com>
2021-12-04 17:26:40 +0000

    Revert "media: Use project pathmap"
    This reverts commit 7bc9760d583efa6803141dff47bc4ab628410b25.

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
-
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -12,6 +10,4 @@ ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 include $(QCOM_MEDIA_ROOT)/libplatformconfig/Android.mk
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
-endif
-
 endif
